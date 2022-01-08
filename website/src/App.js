d8a19ae519cc395cf8f3bf6ad12ae37889ca1806
Kyochul-Jang <jang128@purdue.edu>
2022-01-08 12:33:04 -0500

    Remove ScrollToTop.js.

--- website/src/App.js
+++ website/src/App.js
@@ -4,12 +4,10 @@ import Navbar from "./components/Navbar/Navbar";
 import MainPage from "./components/MainPage/MainPage";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
-import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <ScrollToTop />
       <Navbar />
       <MainPage />
       <Cards />
