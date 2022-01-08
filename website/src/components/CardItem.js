24ac32f3e66f141e92231ca1dc714598ae863243
Kyochul-Jang <jang128@purdue.edu>
2022-01-08 01:12:41 -0500

    Make a card object.

--- website/src/components/CardItem.js
+++ website/src/components/CardItem.js
@@ -4,15 +4,15 @@ function CardItem(props) {
   return (
     <>
       <li className="cards__item">
-        <figure className="cards__item__pic-wrap" data-category={props.Skills}>
+        <figure className="cards__item__pic-wrap" data-category={props.label}>
           <img
-            src={props.CardIamge}
-            alt="Project_Image"
             className="cards__item__img"
+            alt="Travel Image"
+            src={props.src}
           />
         </figure>
         <div className="cards__item__info">
-          <h5 className="cards__item__text">{props.Project_description}</h5>
+          <h5 className="cards__item__text">{props.text}</h5>
         </div>
       </li>
     </>
--- website/src/components/Cards.js
+++ website/src/components/Cards.js
@@ -4,22 +4,20 @@ import CardItem from "./CardItem";
 
 function Cards() {
   return (
-    <>
-      <div className="cards">
-        <h1> My Projects</h1>
-        <div className="cards__container">
-          <div className="cards__wrapper">
-            <ul className="cards__items">
-              <CardItem
-                CardImage="website\src\image\website_project.png"
-                Project_description="My Project"
-                Skills="Java, React"
-              ></CardItem>
-            </ul>
-          </div>
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/website_project.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="HTML, CSS, React.js"
+            />
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
