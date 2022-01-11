a03067c1bdd9cf921c0c00aea733df97f8b9eab9
Kyochul-Jang <jang128@purdue.edu>
2022-01-10 23:04:25 -0500

    Fix cards allignment.

--- website/src/components/CardItem.js
+++ website/src/components/CardItem.js
@@ -3,20 +3,27 @@ import React from "react";
 function CardItem(props) {
   return (
     <>
-      <a href={props.url} target="_blank" rel="noreferrer">
-        <li className="cards__item">
-          <figure
-            className="cards__item__pic-wrap"
-            data-category={props.skills}
-          >
-            <img className="cards__item__img" src={props.src} alt=" " />
-          </figure>
-          <div className="cards__item__info">
-            <h4 className="cards__item__title">{props.title}</h4>
-            <h5 className="cards__item__body">{props.body}</h5>
-          </div>
-        </li>
-      </a>
+      <div className="cards__item__wrapper">
+        <a
+          href={props.url}
+          target="_blank"
+          rel="noreferrer"
+          className="cards__item__atag"
+        >
+          <li className="cards__item">
+            <figure
+              className="cards__item__pic-wrap"
+              data-category={props.skills}
+            >
+              <img className="cards__item__img" src={props.src} alt=" " />
+            </figure>
+            <div className="cards__item__info">
+              <h4 className="cards__item__title">{props.title}</h4>
+              <h5 className="cards__item__body">{props.body}</h5>
+            </div>
+          </li>
+        </a>
+      </div>
     </>
   );
 }
--- website/src/components/Cards.js
+++ website/src/components/Cards.js
@@ -8,22 +8,22 @@ function Cards() {
       <h1>My Projects</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              src="images/website_project.jpg"
-              body="The websites you are viewing now is created by me."
-              title="Website Project"
-              skills="HTML, CSS, React.js"
-              url="https://github.com/OfficerChul/myWebsite"
-            />
-            <CardItem
-              src="images/social_network_profile_project.jpg"
-              body="Network application which can show yourself to others."
-              title="Social Network Profile Project"
-              skills="Java"
-              url="https://github.com/OfficerChul/project-05"
-            />
-          </ul>
+          <CardItem
+            src="images/website_project.jpg"
+            body="The websites you are viewing now is created by me."
+            title="Website Project"
+            skills="HTML, CSS, React.js"
+            url="https://github.com/OfficerChul/myWebsite"
+            className="cards__items"
+          />
+          <CardItem
+            src="images/social_network_profile_project.jpg"
+            body="Network application which can show yourself to others."
+            title="Social Network Profile Project"
+            skills="Java"
+            url="https://github.com/OfficerChul/project-05"
+            className="cards__items"
+          />
         </div>
       </div>
     </div>
