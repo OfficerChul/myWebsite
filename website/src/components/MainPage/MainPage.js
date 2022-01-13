48e1da5a0cd9cd4afcd83ba30f41f4fa0879bcc8
Kyochul-Jang <jang128@purdue.edu>
2022-01-12 19:33:55 -0500

    Add a phrase that this website will continue to be updated.

--- website/src/components/MainPage/MainPage.js
+++ website/src/components/MainPage/MainPage.js
@@ -6,6 +6,9 @@ class MainPage extends Component {
   render() {
     return (
       <div className="main">
+        <div className="stay-tuned">
+          This website will continue to be updated.
+        </div>
         <a href="/" className="logo">
           <img src={logo} className="logo-img" alt=""></img>
         </a>
