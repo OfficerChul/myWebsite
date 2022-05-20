9ba904f165108ceefa704d6c99fec8eee5dfd396
Kyochul-Jang <jang128@purdue.edu>
2022-05-20 11:24:02 +0900

    Add urlItem components

--- src/components/SearchPage/Search.js
+++ src/components/SearchPage/Search.js
@@ -3,6 +3,7 @@ import "./Search.css"
 import logo from "../MainPage/logo_blue.png";
 import googleLogo from "./google.png";
 import magnifier from "./search.png";
+import CircleLink from "./Url/Urls";
 
 export default class Search extends Component {
   render() {
@@ -11,17 +12,17 @@ export default class Search extends Component {
         <section>
             <div className='main'>
               <a href = '/'><img src = {logo} alt='logo' className='logo'></img></a>
-                <div className='searchBox'>
-                    <input className='query' type="search" autocomplete="off" spellcheck="false" 
-                    placeholder="Google Search" aria-live="polite"></input>
-                    <div className='icons'>
-                        <div><img src ={googleLogo} alt='googleLogo' className='googleLogo'></img></div>
-                        <div><button className='magButton'><img src = {magnifier} alt='magnifier'></img></button></div>
-                    </div>
-                </div>
-                {/* <img src = {googleLogo} alt='googleLogo'></img> */}
-                
-                
+              <div className='searchBox'>
+                  <input className='query' type="search" autocomplete="off" spellcheck="false" placeholder="Google Search" aria-live="polite"></input>
+                  <div className='icons'>
+                      <div><img src ={googleLogo} alt='googleLogo' className='googleLogo'></img></div>
+                      <div><button className='magButton'><img src = {magnifier} alt='magnifier'/></button></div>
+                  </div>
+              </div>
+            <div className='circleLinks'>
+              <CircleLink />
+            </div>
+
             </div>
         </section>
       </div>
---
+++ src/components/SearchPage/Url/Urls.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import "./urlItems.css";
+
+export default class urlItems extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
